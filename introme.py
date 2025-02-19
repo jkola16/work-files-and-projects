cfd500d2a56349da266c7ae09a22baa18da7d673
jkola16 <jatinkola99916@gmail.com>
2025-02-19 22:02:22 +0530

    introduction format

---
+++ introme.py
@@ -0,0 +1,5 @@
+# Introduction message
+print("Hello, my name is [Your Name].")
+print("I am passionate about coding and technology.")
+print("I enjoy problem-solving and learning new programming skills.")
+print("Nice to meet you!")
