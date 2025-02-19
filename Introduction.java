e1f200e9bdfb2dd79fe375018717105a44e5d8e0
jkola16 <jatinkola99916@gmail.com>
2025-02-19 22:16:10 +0530

    introduction format in java

---
+++ Introduction.java
@@ -0,0 +1,9 @@
+public class Introduction {
+    public static void main(String[] args) {
+        // Introduction message
+        System.out.println("Hello, my name is [Your Name].");
+        System.out.println("I am passionate about coding and technology.");
+        System.out.println("I enjoy problem-solving and learning new programming skills.");
+        System.out.println("Nice to meet you!");
+    }
+}
